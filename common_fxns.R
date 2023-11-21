e202093c08962261bfa63c473cfae46d081d8540
oharac <casey.c.ohara@gmail.com>
2023-11-21 07:59:43 -0800

    reran LDA with full doc set

--- common_fxns.R
+++ common_fxns.R
@@ -1,17 +1,23 @@
-load_articles <- function(dir = here('_data/bibtex_clean'), pattern = 'wos_', aspect = 'long') {
+load_bibtex <- function(dir = here('_data/bibtex_clean'), pattern = 'wos_', aspect = 'long') {
   bib_clean_fs <- list.files(path = dir, pattern = pattern, full.names = TRUE)
 
+  
   all_fields_df <- lapply(bib_clean_fs, bib2df::bib2df) %>%
-    bind_rows() %>%
-    janitor::clean_names()
+    lapply(janitor::clean_names) %>%
+    setNames(basename(bib_clean_fs)) %>%
+    bind_rows(.id = 'bibtex_source') %>%
+    distinct()
   
   if(aspect == 'long') {
     topic_df <- all_fields_df %>%
-      select(author, journal, year, title, 
-             abstract, keywords, keywords_plus, 
-             web_of_science_categories) %>%
+      select(author, journal, year, title, doi,
+             abstract, contains('keyword'),
+             web_of_science_categories,
+             bibtex_source) %>%
+      distinct() %>%
       mutate(title_protect = title) %>%
-      pivot_longer(cols = c(title, abstract, keywords, keywords_plus), names_to = 'topic', values_to = 'text') %>%
+      pivot_longer(cols = c(title, abstract, web_of_science_categories, contains('keyword')), 
+                   names_to = 'topic', values_to = 'text') %>%
       rename(title = title_protect) %>%
       mutate(text = tolower(text)) %>%
       filter(!is.na(text)) %>%
@@ -23,4 +29,8 @@ load_articles <- function(dir = here('_data/bibtex_clean'), pattern = 'wos_', as
     return(all_fields_df)
   }
   stop('Argument `aspect` must be either "wide" or "long" aspect ratio!')
+}
+
+load_articles <- function() {
+  data.table::fread(here('_data/results_clean.csv'))
 }
